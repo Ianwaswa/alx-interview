6d8c3d0c0bd99ed96cfa45021bda1abfc5c69430
Ianwaswa <ianwaswa2019@gmail.com>
2024-08-06 13:46:28 +0000

    update code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- Pascal's Triangle
+   Pascal's Triangle
 """
 
 def pascal_triangle(n):
@@ -11,9 +11,9 @@ def pascal_triangle(n):
     if n > 0:
         for i in range(1, n + 1):
             level = []
-            C = 1  # The first value in each level is always 1
+            C = 1  # 
             for j in range(1, i + 1):
                 level.append(C)
-                C = C * (i - j) // j  # Compute the next value in the level
+                C = C * (i - j) // j
             res.append(level)
     return res
